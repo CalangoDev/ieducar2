b9e39e9274c6881db811b09a11cae054983550f6
Eduardo Junior <ej@calangodev.com.br>
2015-12-23 15:44:19 -0300

    tipo ensino teste

---
+++ module/Escola/tests/src/Escola/Entity/TipoEnsinoTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Vim.
+ * User: eduardojunior
+ * Date: 23/12/15
+ * Time: 14:08
+ */
+namespace Escola\Entity;
+
+use Core\Entity\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\Factory as InputFactory;
+
+/**
+ * Entidade TipoEnsino
+ * 
