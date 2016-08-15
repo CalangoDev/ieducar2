6d4c4a58eadcbb9a5689248d5255552015759189
Eduardo Junior <ej@calangodev.com.br>
2016-08-14 21:40:10 -0300

    Form Ano Letivo Insert Validation Finish

--- module/Escola/src/Escola/Validator/AnoLetivoModulo.php
+++ module/Escola/src/Escola/Validator/AnoLetivoModulo.php
@@ -15,12 +15,14 @@ class AnoLetivoModulo extends AbstractValidator
     const DATA_INICIO_EMPTY = 'dataInicioEmpty';
     const DATA_FIM_EMPTY = 'dataFimEmpty';
     const MODULO_EMPTY = 'moduloEmpty';
+    const DATA_INICIO_MAIOR_DATA_FIM = 'dataInicioMaiorDataFim';
 
     protected $messageTemplates = [
         self::NOT_NUM => 'Campo só aceita numeros',
         self::DATA_INICIO_EMPTY => 'Campo data de Inicio esta vazio',
         self::DATA_FIM_EMPTY => 'Campo data fim esta vazio',
-        self::MODULO_EMPTY => 'Campo módulo não selecionado'
+        self::MODULO_EMPTY => 'Campo módulo não selecionado',
+        self::DATA_INICIO_MAIOR_DATA_FIM => 'Data de Início não pode ser maior que data fim'
     ];
 
     public function isValid($value)
@@ -28,6 +30,7 @@ class AnoLetivoModulo extends AbstractValidator
         $this->setValue($value);
 
         foreach ($value as $v){
+
             if ('' === $v['dataInicio']){
                 $this->error(self::DATA_INICIO_EMPTY);
                 return false;
@@ -40,6 +43,18 @@ class AnoLetivoModulo extends AbstractValidator
 
             if ('' === $v['modulo']){
                 $this->error(self::MODULO_EMPTY);
+                return false;
+            }
+
+            // formatar datas para poder fazer comparacao
+            $dataInicio = new \DateTime($v['dataInicio'], new \DateTimeZone('America/Sao_Paulo'));
+            $dataInicio->format('Y-m-d');
+
+            $dataFim = new \DateTime($v['dataFim'], new \DateTimeZone('America/Sao_Paulo'));
+            $dataFim->format('Y-m-d');
+
+            if ($dataInicio > $dataFim){
+                $this->error(self::DATA_INICIO_MAIOR_DATA_FIM);
                 return false;
             }
 
